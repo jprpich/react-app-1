f1c64eb8419825b40df59306ddc6ee8909bc46e0
Joshua <joshuaprpich@gmail.com>
2018-02-02 09:49:55 -0500

    Fix error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div>
         <h1>Mi primera aplicación</h1>
-        <p>Esta es mi primera aplicación en React, está padrísimo!".</p>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
       </div>
     );
   }
